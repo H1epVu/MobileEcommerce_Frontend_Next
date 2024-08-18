7e1868a9e31e91a18e0d23c325b70567f1d23c0e
H1ep Vu <hiepvu.tech02@gmail.com>
2024-08-18 15:56:27 +0700

    adjust Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavScroll from "@/components/appNavScroll";
+import Footer from "@/components/app.footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NavScroll />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
